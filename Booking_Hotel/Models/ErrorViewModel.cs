fd32af09c2c34de46d8620ec642be8b58f9eb49e
Nguyen Nhut Quang <nguyennhutquang1304k6@gmail.com>
2026-01-16 21:23:04 +0700

    second add

---
+++ Booking_Hotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Booking_Hotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
